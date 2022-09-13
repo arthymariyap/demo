d991e7d80be55eb2289eae9b68cb2614f0911453
arthymariyap <113424428+arthymariyap@users.noreply.github.com>
2022-09-13 18:28:06 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 	
 	@GetMapping
 	public String sayHello() {
-		return "The website is up and running first commit";
+		return "The website is up and running first commit !!!";
 	}
 
 }
